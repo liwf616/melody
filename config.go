62e8d276f59970f765a5377c39f83b5530b652cd
wf.li <liwf616@gmail.com>
2019-04-11 20:15:14 +0800

    MaxMessageSize

--- config.go
+++ config.go
@@ -16,7 +16,7 @@ func newConfig() *Config {
 		WriteWait:         10 * time.Second,
 		PongWait:          60 * time.Second,
 		PingPeriod:        (60 * time.Second * 9) / 10,
-		MaxMessageSize:    512,
+		MaxMessageSize:    512 * 1024,
 		MessageBufferSize: 256,
 	}
 }
